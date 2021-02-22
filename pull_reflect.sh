01621d3b73bf6242ff11d785eac34464d3615699
中本昌吾 <sh05@sh05MBP.local>
2021-02-22 20:27:33 +0900

    add scripts to reflect

---
+++ pull_reflect.sh
@@ -0,0 +1,4 @@
+\cp -f ./.zshrc ../
+\cp -f ./.vimrc ../
+\cp -f ./dein.toml ../.vim/rc/
+\cp -f ./dein_lazy.toml ../.vim/rc/
---
+++ push_reflect.sh
@@ -0,0 +1,4 @@
+\cp -f ../.zshrc ./
+\cp -f ../.vimrc ./
+\cp -f ../.vim/rc/dein.toml ./
+\cp -f ../.vim/rc/dein_lazy.toml ./
