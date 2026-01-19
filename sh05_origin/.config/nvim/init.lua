fcc04cf1657d2e38d5b6c49807608c60c37cf2f3
sh05 <shogonakamoto0107@gmail.com>
2026-01-19 23:00:35 +0900

    remove old files

--- sh05_origin/.config/nvim/init.lua
+++
@@ -1,8 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-require("lazy").setup({}, {
-  rocks = {
-    hererocks = true, -- Hererocksを有効化
-    enabled = false, -- Luarocksを無効化
-  },
-})
--- sh05_origin/.config/nvim/lua/plugins/filer.lua
+++
@@ -1,9 +0,0 @@
-require('neo-tree').setup {
-    filesystem = {
-        filtered_items = {
-            visible = true,  -- Show hidden files
-            hide_dotfiles = false,  -- Do not hide dotfiles
-            hide_gitignored = true,  -- Optionally hide gitignored files
-        },
-    },
-}
